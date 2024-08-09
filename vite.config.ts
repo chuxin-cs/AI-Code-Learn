935632c18c049a97baf9e066441c9afaa3c3fbcd
chuxin <15115359055@139.com>
2024-08-09 17:38:26 +0800

    feat: 实现业务中的新 AppRouteObject 类型

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // 这里就是需要配置resolve里的别名
+      "@": path.join(__dirname, "./src"),
+      "#": path.join(__dirname, "./types"),
+    },
+  },
   server: {
     port: 3000,
-    proxy: {},
   },
 });
