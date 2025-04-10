21d4926223d33ca99365169c2ab968cc67345fa9
chuxin <15115359055@139.com>
2025-04-10 13:35:05 +0800

    feat: 添加@tocc/constants包并更新相关依赖
    新增@tocc/constants包，用于存放常量。在App.tsx中引入并使用USER_NAME常量，同时在package.json中添加@tocc/constants依赖并更新pnpm-lock.yaml。

--- apps/main/src/App.tsx
+++ apps/main/src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { isString } from '@tocc/shared/utils';
+import { USER_NAME } from '@tocc/constants';
 
 function App() {
   const [count, setCount] = useState(0);
-  console.log('===>', isString(123));
+  console.log('@tocc/shared/utils===>', isString(123));
+  console.log("@tocc/constants===>",USER_NAME)
 
   return (
     <>
