cce4b27dbf3e5feb50fa0f6509da52a6ce12153a
chuxin <15115359055@139.com>
2025-04-14 14:47:43 +0800

    refactor(main): 优化项目配置和根组件渲染逻辑
    - 在 `vite.config.ts` 中增加路径别名配置，简化模块导入
    - 在 `tsconfig.app.json` 中配置 `baseUrl` 和 `paths`，支持路径别名
    - 在 `main.tsx` 中使用 `HelmetProvider` 和 `Suspense` 增强根组件渲染

--- apps/main/src/main.tsx
+++ apps/main/src/main.tsx
@@ -1,8 +1,13 @@
+// react
+import { Suspense } from "react";
 import { createRoot } from 'react-dom/client';
+// helmet
+import { HelmetProvider } from 'react-helmet-async';
+// root component
 import App from './App.tsx';
 
 // qiankun 相关配置 后期会迁移到 packages 目录中
-import {registerMicroApps, start} from "qiankun";
+import { registerMicroApps, start } from 'qiankun';
 
 registerMicroApps([
   {
@@ -16,15 +21,19 @@ registerMicroApps([
     entry: '//localhost:9002',
     container: '#baseContainer',
     activeRule: '/base',
-  }
-])
-
+  },
+]);
 
 let root = null;
 function init() {
   root = createRoot(document.getElementById('root')!);
-  root.render(<App />);
+  root.render(
+    <HelmetProvider>
+      <Suspense>
+        <App />
+      </Suspense>
+    </HelmetProvider>
+  );
   start();
-  console.log("main 服务init...")
 }
 init();
